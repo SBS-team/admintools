3dcf154ae7b066ed9ceb89d3afb969513959b6c3
valikos <valikos.ost@gmail.com>
2012-08-14 15:26:44 +0300

    fix user model chenge relation with room from belongs_to :room to has_one :room

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :info, :last_name, :skype
 
-  has_one    :desktop
-  has_many   :devices
-  belongs_to :room
+  has_one  :desktop
+  has_many :devices
+  has_one  :room
   
   validates :first_name, :presence => true
   validates :last_name,  :presence => true
   validates :email,      :presence => true, :uniqueness => true
   validates :skype,      :presence => true
-
 end
