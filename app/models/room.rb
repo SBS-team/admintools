49f190d7871a83fbeade0f21ecd87c21983a6829
Viktor <viktor.markevich@faceit.com.ua>
2012-08-16 11:40:12 +0300

    task#198
    add rename a field office in the name, change the type to string

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,11 @@
 class Room < ActiveRecord::Base
-  attr_accessible :office, :user_id
+  attr_accessible :name, :user_id
 
   has_many :desktops
   has_many :users, :through => :desktops
   belongs_to :user
 
-  validates :office, :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true
   validates :user_id, :presence => true, :uniqueness => true
+
 end
