65df5835cb3bdea1a0312c34e72d12c5536968a8
valikos <valikos.ost@gmail.com>
2012-08-17 09:35:45 +0300

    Create room desktops counter

--- app/models/desktop.rb
+++ app/models/desktop.rb
@@ -1,11 +1,24 @@
 class Desktop < ActiveRecord::Base
+  before_save :dec_counter
+  after_save :inc_counter
+  
   attr_accessible :info, :ip, :mac, :name
 
-  belongs_to :room
+  belongs_to :room, :counter_cache => true
   belongs_to :user
 
   validates :ip,  :presence => true, :uniqueness => true, :format => {:with => /\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/, :massage => 'Wrong IP'}
   validates :mac, :presence => true, :uniqueness => true, :format => {:with => /\b([A-Fa-f0-9]{2}[:-]){5}[A-Fa-f0-9]{2}\b/, :massage => 'Wrong MAC'}
   validates :user_id, :uniqueness => true, :if => :user_id?
   validates :name, :presence => true
+
+  private
+
+  def dec_counter
+    Room.update(room_id, {:desktops_count => Room.find(room_id).desktops.count - 1}) if :room_id?
+  end
+
+  def inc_counter
+    Room.update(room_id, {:desktops_count => Room.find(room_id).desktops.count + 1}) if :room_id?
+  end
 end
