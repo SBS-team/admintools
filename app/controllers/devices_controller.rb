598fd75f0592f84d789d0e6d8c6010b08955da42
Viktor <viktor.markevich@faceit.com.ua>
2012-08-22 15:03:26 +0300

    224_sorting_devices
    add sorting for devives controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,10 +1,11 @@
 class DevicesController < ApplicationController
   before_filter :users_all, :only => [:index, :new, :edit, :create, :update]
   before_filter :current_device, :only => [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
     @search = Device.search(params[:search])
-    @devices = @search.order('created_at').all
+    @devices = @search.order(sort_column+ " " + sort_direction).all
   end
 
   def new
@@ -48,6 +49,14 @@ class DevicesController < ApplicationController
 
   def current_device
     @device = Device.find_by_id(params[:id])
+  end
+
+  def sort_column
+    Device.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
 
 end
