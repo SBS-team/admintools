b0417c1904d6618c8719aa773dc3a3b3c3e3a3a4
Viktor <viktor.markevich@faceit.com.ua>
2012-08-22 15:17:02 +0300

    224_sorting_user
    add sorting for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   before_filter :current_user, :only => [:show, :edit, :update, :destroy]
+  helper_method :sort_column, :sort_direction
 
   def index
     @search = User.search(params[:search])
-    @users = @search.order('created_at').all
+    @users = @search.order(sort_column+ " " + sort_direction).all
   end
 
   def new
@@ -44,4 +45,13 @@ class UsersController < ApplicationController
   def current_user
     @user = User.find_by_id(params[:id])
   end
+
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
