489a76759fd31edfd64dc4e61b33e87e76bf2bf7
Viktor <viktor.markevich@faceit.com.ua>
2012-08-28 10:36:59 +0300

    device
    add test for association

--- spec/models/desktop_spec.rb
+++ spec/models/desktop_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
   describe Desktop do
-    it { should belong_to(:room)  }
+    it { should belong_to(:room) }
     it { should belong_to(:user) }
-    it { should have_one(:workplace).dependent(:nullify)}
+    it { should have_one(:workplace).dependent(:nullify) }
   end
