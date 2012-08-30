f02f12b0a198638d1ca3fe9d1c66df961113b66e
Viktor <viktor.markevich@faceit.com.ua>
2012-08-30 14:57:18 +0300

    room_plan_test
    add test for check eq room.id & room_id into room_plan

---
+++ spec/factories/room_plane.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :room_plan do
+    association :room
+  end
+end
--- spec/models/room_plan_spec.rb
+++ spec/models/room_plan_spec.rb
@@ -4,4 +4,14 @@ require "spec_helper"
     it { should belong_to :room }
     it { should have_many :workplaces }
     it { should have_many(:desktops).through(:workplaces) }
+
+    context "creating room" do
+      before :each do
+        @room = FactoryGirl.create :room
+        @room_plan = FactoryGirl.create :room_plan, :room => @room
+      end
+      it "should create room with id eq room_id" do
+        @room.reload.id.should == @room_plan.room_id
+      end
+    end
   end
