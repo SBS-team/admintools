1c3775ff97871780f0ea1e47b793fa7628908610
BlackKOT <oko10ko@ukr.net>
2012-08-30 11:49:35 +0300

    add edit users on calendar part1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,22 +1,17 @@
 class Event < ActiveRecord::Base
-  attr_accessor :send_to_admins
 
-  attr_accessible :title, :starts_at, :ends_at, :all_day, :description, :send_at, :sended, :send_to_admins
-  has_many :event_admins
+  has_many :event_admins, :dependent => :delete_all
   has_many :admins, :through => :event_admins
 
   validate :starts_at_is_less_than_ends_at
 
-  attr_accessor :send_to_users
+  accepts_nested_attributes_for :event_admins
+  attr_accessor :send_to_admins
 
-  attr_accessible :title, :starts_at, :ends_at, :all_day, :description, :send_at, :sended, :send_to_users
-
-  scope :before, lambda {|end_time| {:conditions => ["ends_at < ?", Event.format_date(end_time)] }}
-  scope :after, lambda {|start_time| {:conditions => ["starts_at > ?", Event.format_date(start_time)] }}
+  scope :before, lambda {|end_time| where("ends_at < ?", Event.format_date(end_time))}
+  scope :after, lambda {|start_time| where("starts_at > ?", Event.format_date(start_time))}
   scope :sending_event, lambda { where("send_at < ?", Time.zone.now).where("send_at > ?", Time.zone.now - 2.month).where(:sended => false) }
 
-  # need to override the json view to return what full_calendar is expecting.
-  # http://arshaw.com/fullcalendar/docs/event_data/Event_Object/
   def as_json(options = {})
     {
         :id => self.id,
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,12 @@ class User < ActiveRecord::Base
   has_many :devices
   has_one :room, :through => :desktop
 
-  attr_reader :user_tokens
-
   validates :first_name, :presence => true
   validates :last_name,  :presence => true
   validates :email,      :presence => true, :uniqueness => true
   validates :skype,      :presence => true
 
   attr_reader :user_tokens
-
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}
 
   def full_name
