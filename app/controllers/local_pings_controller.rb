5750e46ac86e49815eba22b4680046aefaf50e64
valikos <valikos.ost@gmail.com>
2012-09-04 16:52:45 +0300

    fix local_ping controllers and views for polymorphic relation

--- app/controllers/local_pings_controller.rb
+++ app/controllers/local_pings_controller.rb
@@ -1,10 +1,10 @@
 class LocalPingsController < ApplicationController
   def index
-    @pings = Desktop.all 
+    @logs = Desktop.all
   end
 
   def show 
     @desktop = Desktop.find_by_ip(params[:ip])
-    @pings = @desktop.local_pings.where(:up => Time.now.utc.midnight..Time.now.utc.end_of_day)
+    @pings = @desktop.ping_logs.where(:up => Time.now.utc.midnight..Time.now.utc.end_of_day)
   end
 end
