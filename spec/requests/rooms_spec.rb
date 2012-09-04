084b544bee6d96908e3e3d86315b20a60dbed882
Viktor <viktor.markevich@faceit.com.ua>
2012-09-04 14:32:43 +0300

    add_test
    add test for search form

--- spec/requests/rooms_spec.rb
+++ spec/requests/rooms_spec.rb
@@ -13,9 +13,23 @@ describe "Rooms" do
   context "room creating", :js => true do
     before(:each) do
       user_creating
+      visit rooms_path
     end
+
     it "room have content" do
-      visit users_path
+      page.should have_content("Vasya Ivanov")
+      page.should have_link("Конструктор")
+      page.should have_link("Просмотр")
+      page.should have_link("Редактировать")
+      page.should have_link("Удалить")
+    end
+
+    it "room have search form" do
+      within "#room_search" do
+        fill_in "Введите имя офиса", :with => "1"
+        select @user.full_name
+      end
+      click_button('Search')
       page.should have_content("Vasya Ivanov")
     end
   end
