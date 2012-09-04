ca3a60cdc9c12aabf4078848d0b3da105485a7b2
Viktor <viktor.markevich@faceit.com.ua>
2012-09-04 17:34:10 +0300

    controller
    add actions into domains controller

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,6 +1,43 @@
+#encoding=UTF-8
 class DomainsController < ApplicationController
+  before_filter :current_domain, :only => [:show, :edit, :update, :destroy]
 
   def index
+    @domains = Domain.all
+  end
 
+  def new
+    @domain = Domain.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @domain = Domain.new(params[:domain])
+    if @domain.save
+      redirect_to :domains, :notice => "Домен #{@domain.url} добавлен"
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    if @domain.update_attributes(params[:domain])
+      redirect_to :domains, :notice => "Домен #{@domain.url} обновлен"
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    @domain.destroy and redirect_to :domains, :notice => "Домен #{@domain.url} удален"
+  end
+
+  private
+
+  def current_domain
+    @domain = Domain.find(params[:id])
   end
 end
