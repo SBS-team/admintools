517327425384f3616a1bfcbad6f3af778640de56
Viktor <viktor.markevich@faceit.com.ua>
2012-09-06 16:33:42 +0300

    model_groop
    add find_in_batches for the formation of the group url

--- app/jobs/domain_jobs_ping.rb
+++ app/jobs/domain_jobs_ping.rb
@@ -2,9 +2,11 @@ class DomainJobsPing
   @queue = :domain_jobs_ping
 
   def self.perform()
-    curl = DomainPing.send_ping(Domain.all)
-    curl.each_pair do |k, v|
-      Domain.find_by_url(k).update_attributes(:active => v)
+    Domain.find_in_batches(:batch_size => 2) do |dom|
+      curl = DomainPing.send_ping(dom)
+      curl.each_pair do |k, v|
+        Domain.find_by_url(k).update_attributes(:active => v)
+      end
     end
   end
 
