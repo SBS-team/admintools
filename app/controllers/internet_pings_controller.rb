eb7ca73a6366fd2f8285fd94bfd897932f09b764
Viktor <viktor.markevich@faceit.com.ua>
2012-09-18 14:23:10 +0300

    remove_excess

--- app/controllers/internet_pings_controller.rb
+++ app/controllers/internet_pings_controller.rb
@@ -1,18 +1,7 @@
 class InternetPingsController < ApplicationController
-  before_filter :domains_all, :only => [:index]
-
   def index
     @search = PingLog.where(:ping_type => "Domain").search(params[:search] || {"meta_sort" => 'id.asc'})
     @internet_pings = @search.paginate(:page => params[:page]).all
-  end
-
-  def show
-  end
-
-  private
-
-  def domains_all
     @domains ||= Domain.all
   end
-
 end
