457dbdfc024af9bf3a017d1c1d0e17bbc0f08365
valikos <valikos.ost@gmail.com>
2012-09-24 11:05:02 +0300

    Added styled for error messages, validation for url in domain model

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,5 +1,5 @@
 class Domain < ActiveRecord::Base
   has_many :ping_logs, :as => :ping
-
   self.per_page = 10
+  validates :url, :presence => true, :uniqueness => true, :format => {:with => /^(?:http\:[\/]{2}[w]{3}){1}(?:\.[_\-a-zA-Z0-9\/\.]{3,})$/, :massage => 'Wrong URL'}
 end
