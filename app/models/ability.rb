9d119ac823e8501be857a9267e35c0bf030b6fcb
valikos <valikos.ost@gmail.com>
2012-10-19 17:04:46 +0300

    Fixed cancan access

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,11 @@ class Ability
 
   def initialize(user)
 
-    if user.role == "user"
-      can :read, :except => [UserChange]
+    if user.is_user?
+      can :read, :except => UserChange
     end
 
-    if user.role == 'teamleader'
+    if user.is_teamleader?
       can :manage, User.where(:department_id => user.department_id)
       can :read, UserChange
     end
@@ -18,10 +18,6 @@ class Ability
       can :read, UserChange
     end
 
-    can :manage, :all if user.role == "admin"
-
-    # can :manage, User if user.role == 'manager'
-    # can :manage, Department if user.role == 'manager'
-    # can :manage, UserChange if user.role == 'manager'
+    can :manage, :all if user.is_admin?
   end
 end
