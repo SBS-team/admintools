cfbaa89a0df51efe4c00faae0ac3e12e468c38d0
sedzen <sedzen@kartamyshev.(none)>
2012-10-24 18:43:20 +0300

    done report mailer.

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,9 +2,9 @@ class ReportMailer < ActionMailer::Base
   default from: "dmitriy.savran@faceit.com.ua"
 
   def send_report_mail(teamleader_report, temleader_users_reports)
-    @teamleader=@teamleader_report.user
     @teamleader_report=teamleader_report
     @teamleader_users_reports=temleader_users_reports
+    @teamleader=@teamleader_report.user
     addresses=User.where(:role=>'director').select(:email)
     mail(:to => addresses.map(&:email), :subject => "Monthly report from #{@teamleader.full_name} (#{@teamleader.department.name})", :template_path => 'teamleader/report_mailer',:template_name => 'send_report_mail')
   end
