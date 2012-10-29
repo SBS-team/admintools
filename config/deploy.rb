3ad7a2f08f3eddaccefeba542e09991360d70fe6
BlackKOT <oko10ko@ukr.net>
2012-10-29 13:54:45 +0200

    capistrano with autorun unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,12 @@ set :domain, "192.168.137.34"
 set :repository, "git@github.com:is-valid/admintools.git"
 set :rails_env, "production"
 #set :branch, "teamleader"
-set :branch, "update_and_fix_bootstrap"
+#set :branch, "update_and_fix_bootstrap"
+set :branch, "capistrano_release_one"
 #set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения. Очень актуально для больших и тяжелых репозитариев.
 set :user, "dimon"
 set :use_sudo, false
+set :deploy_to, "/home/dimon/web/apps/admintools"
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
@@ -22,18 +24,16 @@ role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
 
-set :deploy_to, "/home/dimon/web/apps/admintools"
-
 before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby' # интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
 
 
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{release_path} && bundle exec unicorn -E #{rails_env} -D; fi"
   end
   task :start do
-    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
-    #run "bundle exec unicorn -E production"
+    #run "cd #{release_path}; bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+    run "cd #{release_path}; bundle exec unicorn -E production"
   end
   task :stop do
     run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
@@ -44,9 +44,10 @@ after "deploy:update_code",
       "deploy:config_symlink",
       "deploy:db_create",
       "deploy:db_migrate",
-      "deploy:db_seed"
-      #"deploy:bundle_install_no_dev",
+      "deploy:db_seed",
+      "deploy:bundle_install_no_dev"
       #"deploy:start"
+
 
 namespace :deploy do
   task :config_symlink do
@@ -63,18 +64,18 @@ namespace :deploy do
     run "cd #{release_path}; RAILS_ENV=production rake db:seed"
   end
   task :bundle_install_no_dev, :roles => :app do
-    #run "cd #{release_path}; bundle install --no-deployment"
+    run "cd #{release_path}; bundle install --no-deployment"
   end
 
 
 end
 
-after 'deploy:update_code', :roles => :app do
+#after 'deploy:update_code', :roles => :app do
   # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
   #run "rm -f #{current_release}/config/database.yml"
   #run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
   #run "cd #{release_path}; rake assets:precompile RAILS_ENV=production "
-end
+#end
 
 #ssh_options[:port] = 7822
 #set :repository, "svn+ssh://root@localhost/var/www/test.com"
@@ -97,10 +98,10 @@ after "deploy:restart", "deploy:cleanup"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     #run touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
-end
+#namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     #run touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+#end
