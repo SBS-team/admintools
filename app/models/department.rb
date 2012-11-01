c52252aca493b73a8466c7f034de398c008d151c
valikos <valikos.ost@gmail.com>
2012-11-01 16:24:29 +0200

    Fixed validation localization for User and Department models TODO fix validation loclisation for all models

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,8 @@
 class Department < ActiveRecord::Base
   # attr_accessible :title, :body
-  validates :name, :presence => true, :uniqueness => true
+  validates :name, :presence => true,
+                   :uniqueness => { :case_sensitive => false }
+
 
   acts_as_paranoid
 
