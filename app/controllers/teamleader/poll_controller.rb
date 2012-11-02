9694029c639155f11a39ddf241108b66df2fd0f3
BlackKOT <oko10ko@ukr.net>
2012-11-02 18:18:20 +0200

    change mail text

--- app/controllers/teamleader/poll_controller.rb
+++ app/controllers/teamleader/poll_controller.rb
@@ -26,7 +26,7 @@ class Teamleader::PollController < Teamleader::AppTeamleaderController
     if !params['poll']['question'].blank? && params['poll']['max_votes'].to_i > 0 && params['poll']['option'].delete_if { |x| x == "" }.count > 1
       @poll = Poll.create(params['poll'])
       @poll.update_attribute(:end_at, 1.day.from_now) if @poll.end_at < DateTime.now
-      PollMailer.send_poll_mail(@poll).deliver
+      PollMailer.send_poll_mail(@poll,current_user).deliver
       redirect_to teamleader_poll_index_path
     else
       flash[:alert] = t(:'teamleader.poll.create.invalid')
