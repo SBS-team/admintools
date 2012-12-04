71450743c3fc1e9808eff346c3c629228ac50aa1
Viktor <viktor.markevich@faceit.com.ua>
2012-12-04 17:32:45 +0200

    fixed absent_mailer

--- app/mailers/absent_mailer.rb
+++ app/mailers/absent_mailer.rb
@@ -4,8 +4,8 @@ class AbsentMailer < ActionMailer::Base
   def send_absent_mail(absent)
     @absent=absent
     @user=absent.user
-    addresses = User.user_teamleader(@user).select(:email)
-    mail(:to => addresses.map(&:email)+["manager@faceit.com.ua"], :subject => t(:'teamleader.absent_mailer.send_absent_mail.subject'), :template_path => 'teamleader/absent_mailer',:template_name => 'send_absent_mail')
+    addresses = User.where(:role => ["manager", "admin"]).pluck(:email) << User.user_teamleader(@user).first.email
+    mail(:to => addresses, :subject => t(:'teamleader.absent_mailer.send_absent_mail.subject'), :template_path => 'teamleader/absent_mailer',:template_name => 'send_absent_mail')
   end
 end
 
