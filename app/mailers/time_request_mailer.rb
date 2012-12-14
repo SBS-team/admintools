f73a271885a06107e2bcd2691e88a8a2ae9ff6f2
Jenua Boiko <jeyboy@bigmir.net>
2012-12-14 18:32:54 +0200

    fix user notify title

--- app/mailers/time_request_mailer.rb
+++ app/mailers/time_request_mailer.rb
@@ -9,6 +9,6 @@ class TimeRequestMailer < ActionMailer::Base
   def time_request_response(time_request, old_time = nil)
     @time_request=time_request
     @old_time = old_time
-    mail(:to => time_request.user.email, :subject => t(:'teamleader.mailer.time_request_mailer.new_request'), :template_path => 'teamleader/time_request_mailer', :template_name => 'time_request_response')
+    mail(:to => time_request.user.email, :subject => t(:'teamleader.mailer.time_request_mailer.request_response'), :template_path => 'teamleader/time_request_mailer', :template_name => 'time_request_response')
   end
 end
