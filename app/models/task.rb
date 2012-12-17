579248700d4bc20554f33d16c1d2ee72be937c29
Viktor <viktor.markevich@faceit.com.ua>
2012-12-17 14:38:34 +0200

    added connection between models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
   attr_accessible :creator_id, :description, :execution_date, :priority, :task, :title, :user_id
 
-
+  belongs_to :user
+  belongs_to :creator, :class_name => "User", :foreign_key => "creator_id"
 end
