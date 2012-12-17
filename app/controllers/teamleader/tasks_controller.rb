f2b6bc731b074307f821785a09db225e2b89436b
markevich <viktor.markevich@faceit.com.ua>
2012-12-17 17:05:15 +0200

    added destroy method; fixed create method

--- app/controllers/teamleader/tasks_controller.rb
+++ app/controllers/teamleader/tasks_controller.rb
@@ -9,12 +9,18 @@ class Teamleader::TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(params[:task])
-    @task.creator_id << current_user
+    @task = Task.new(params[:task].update(:creator_id => current_user.id ))
     if @task.save
-     redirect_to @task, notice: 'Task was successfully created.'
+     redirect_to teamleader_tasks_path, notice: 'Task was successfully created.'
     else
       render action: "new"
     end
   end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to teamleader_tasks_path, notice: 'Task was successfully deleted.'
+  end
+
 end
