00b29d4687356bab93b154a1eabc85e229f4711a
zadvorniy_s <snz985@gmail.com>
2012-12-18 12:47:10 +0200

    test
    added tests (admin views)

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -1,11 +1,11 @@
 #encoding=UTF-8
 require 'spec_helper'
 
-describe "Admin" do
-
-  context "admin view", :js => true do
+describe "Admin" do  #, :js => true
+  context "admin view" do
     before(:each) do
       login_admin(:super_admin)
+      (1..5).each {|i| FactoryGirl.create(:admin, :name => "name#{i}", :email => "email#{i}@blabla.com")}
       visit admin_admins_path
     end
 
@@ -17,6 +17,55 @@ describe "Admin" do
       page.should have_link("Редактировать")
       page.should have_link("Удалить")
       page.should have_link("Удаленные")
+    end
+  end
+
+  describe "admin search" do
+    before(:each) do
+      login_admin(:super_admin)
+      (1..5).each {|i| FactoryGirl.create(:admin, :name => "name#{i}", :email => "email#{i}@blabla.com")}
+      visit admin_admins_path
+    end
+
+    it "search admin name" do
+      page.should have_content("blabla")
+    end
+
+    it "search admin name" do
+      within "#admin_search" do
+        fill_in 'search_name_equals', :with => "name2"
+      end
+      click_button('Найти')
+      page.should have_content("name2")
+      page.should have_content("email2@blabla.com")
+      page.should_not have_content("email3@blabla.com")
+    end
+
+    it "search admin email" do
+      within "#admin_search" do
+        fill_in 'search_email_equals', :with => "email3@blabla.com"
+      end
+      click_button('Найти')
+      page.should have_content("name3")
+      page.should have_content("email3@blabla.com")
+      page.should_not have_content("email4@blabla.com")
+    end
+
+    it "search admin abort" do
+      within "#admin_search" do
+        fill_in 'search_name_equals', :with => "name3"
+        fill_in 'search_email_equals', :with => "email3@blabla.com"
+      end
+      click_link('Сбросить')
+      page.should have_content("email3@blabla.com")
+      page.should have_content("email4@blabla.com")
+    end
+  end
+
+  describe "Managing аdmins" do
+    before(:each) do
+      login_admin(:super_admin)
+      visit admin_admins_path
     end
 
     it "creating new admin" do
@@ -31,6 +80,18 @@ describe "Admin" do
       click_button('Создать')
       page.should have_content("vasya")
       page.should have_content("vasya@qwe.com")
+    end
+
+    #it "destroy to admin"
+
+    it "show admin" do
+      click_link("Просмотр")
+      page.should have_content("Информация пользователя")
+    end
+
+    it "update to admin" do
+      click_link("Редактировать")
+      page.should have_content("Изменить пользователя")
     end
   end
 end
